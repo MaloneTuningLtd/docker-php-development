ff21c03d0ccc8ae2e3932e276382c24549d78fa3
Sean <seantdwilson@gmail.com>
2018-08-02 10:26:18 -0700

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# PHP Development Dockerfile
+
+## Features
+
+- extensions so stuff works
+- composer
+- [wait-for-it.sh](https://github.com/vishnubob/wait-for-it)
+
+## Updating Images
+
+Images are generated from the `Dockerfile.template` file. Modifications to this template can be propagated by running `generate.sh`.
+
---
+++ generate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+declare -a versions=(
+        "latest"
+        "7.2"
+        "7.1"
+        "7.0"
+    )
+
+basePath="generated"
+
+for version in "${versions[@]}"
+do
+    versionFolder="$basePath/$version"
+    dockerFilePath="$versionFolder/Dockerfile"
+
+    # make versionFolder path if it doesn't exist
+    mkdir -p "$versionFolder"
+
+    # remove existing Dockerfile if it exists
+    if [ -f "$dockerFilePath" ]; then
+        rm "$dockerFilePath"
+    fi
+
+    # generate new Dockerfile
+    cat HEADER > "$dockerFilePath"
+    cat Dockerfile.template | sed -e "s/\$VERSION/$version/g" >> "$dockerFilePath"
+
+    echo "Generated $version"
+done
+
