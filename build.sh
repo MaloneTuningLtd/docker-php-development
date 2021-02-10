01f2ae15f1146487716e3fe3fc3cdf505f473abb
Sean <sean@albinodrought.com>
2021-02-10 11:02:05 -0800

    feat!: drop 7.0, 7.1 support
    [CI SKIP]

--- build.sh
+++ build.sh
@@ -4,6 +4,4 @@ docker build -t maloneweb/docker-php-development:8.0 generated/8.0 &
 docker build -t maloneweb/docker-php-development:7.4 generated/7.4 &
 docker build -t maloneweb/docker-php-development:7.3 generated/7.3 &
 docker build -t maloneweb/docker-php-development:7.2 generated/7.2 &
-docker build -t maloneweb/docker-php-development:7.1 generated/7.1 &
-docker build -t maloneweb/docker-php-development:7.0 generated/7.0 &
 wait
